7f72458e3e7557b8385dcca505519df6fbae583c
albertolab <albertolab20@gmail.com>
2024-04-19 17:38:05 -0400

    Initial commit

---
+++ src/app/rutas-autobuses/page.tsx
@@ -0,0 +1,3 @@
+export default function RoutesPage() {
+  return <>hola que tal</>;
+}
---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,17 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Rutas de Autobuses",
+    path: "/rutas-autobuses",
+    newTab: false,
+  },
+];
+export default menuData;
